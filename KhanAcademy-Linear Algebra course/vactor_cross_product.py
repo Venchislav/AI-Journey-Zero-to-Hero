f3f28df553b1566af1bb8bece3b9dd9174a98a21
Venchislav <97972996+Venchislav@users.noreply.github.com>
2024-03-10 17:00:16 +0300

    That's it

---
+++ KhanAcademy-Linear Algebra course/vactor_cross_product.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+vector_w = [1, 2, 5]
+vector_v = [3, 4]
+
+print(np.cross(vector_w, vector_v))
+
+
+"""
+btw, I'm switching the course on course about linear algebra for machine learning
+as I've heard more good things about it. That's what I need.
+I need to grow up as a ML engineer, and then start growing wide (There's a cool book about math for ML)
+
+Mathematics for Machine Learning
+"""
