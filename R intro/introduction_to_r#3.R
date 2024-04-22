f644178a8cda9bccfed0ce3612124e2d0318ef89
Venchislav <97972996+Venchislav@users.noreply.github.com>
2024-04-22 19:18:19 +0300

    Dataset loaded (with R)
    R sucks, but it's good...
    
    I mean in real ML engineers don't use R

---
+++ R intro/introduction_to_r#3.R
@@ -0,0 +1,12 @@
+library(datasets)
+
+?mtcars
+
+# The data was extracted from the
+# 1974 Motor Trend US magazine,
+# and comprises fuel consumption and
+# 10 aspects of automobile design and
+# performance for 32 automobiles
+# (1973–74 models).
+
+head(mtcars)
