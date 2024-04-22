05c77ee91a68727e0e2d232c096d6cee98cf60ac
Venchislav <97972996+Venchislav@users.noreply.github.com>
2024-04-22 19:37:33 +0300

    Barplots theory
    We use barplots to describe categorical data.
    Next lesson is about Histograms (way of plotting continuous data)

--- R intro/introduction_to_r#3.R
+++ R intro/introduction_to_r#3.R
@@ -10,3 +10,27 @@ library(datasets)
 # (1973–74 models).
 
 head(mtcars)
+
+barplot(mtcars$cyl)  # sh🎸t as it builds plot of cylinders distribution
+# we don't give a duck about it
+
+# so let's create variable of special data type (table)
+cylinders <- table(mtcars$cyl)  # create cylinders data object
+
+barplot(cylinders)  # this works fine (it's kind of count plot of our cylinders)
+plot(cylinders)  # same plot, but with lines instead of bars
+
+
+# yeah... R is trash (remember?)
+# so we assign variables here in
+# UGLY way...
+
+pisya <- 'Hello world!'
+print(pisya)  # Hello world!
+
+pisya = 'Goodbye world!'
+print(pisya)  # Goodbye world!
+
+# Thanks to God! I thought that ugly thing is the only way...
+
+
